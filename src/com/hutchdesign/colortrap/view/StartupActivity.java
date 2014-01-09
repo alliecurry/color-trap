85d2da923a542cebd5b7c7e0d1436fc598270d1f
Allie Curry <allie@spantree.net>
2014-01-08 20:51:13 -0600

    Added basic menu for starting the game

--- src/com/hutchdesign/colortrap/view/StartupActivity.java
+++ src/com/hutchdesign/colortrap/view/StartupActivity.java
@@ -3,20 +3,29 @@ package com.hutchdesign.colortrap.view;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import com.hutchdesign.colortrap.R;
 
 public class StartupActivity extends Activity {
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
 
-        // TODO show menu, start button creates new grid
+        View newGame = findViewById(R.id.menu_text_new);
+        newGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGame();
+            }
+        });
 
+    }
+
+    // TODO send bundle with game state
+    private void startGame() {
         Intent i = new Intent(this, ColorTrap.class);
         startActivity(i);
-//       GridView gridView = (GridView) findViewById(R.id.gridview);
-//       gridView.setNumColumns(5);
-//       gridView.setAdapter(new GridAdapter(this));
     }
 }
