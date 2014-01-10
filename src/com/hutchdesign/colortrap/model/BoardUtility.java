7e7504cb6e5aabfae68d9c07eff902f6151ac8b4
Allie Curry <allie@spantree.net>
2014-01-10 16:02:21 -0600

    Utility class now follows proper utility standards for initialization

--- src/com/hutchdesign/colortrap/model/BoardUtility.java
+++ src/com/hutchdesign/colortrap/model/BoardUtility.java
@@ -3,6 +3,11 @@ package com.hutchdesign.colortrap.model;
 /**
  * Created by mike.hutcheson on 1/5/14.
  */
-public class BoardUtility {
+public final class BoardUtility {
+
+    private BoardUtility() {
+        throw new AssertionError();
+    }
+
     //Move gameboard utility methods here at some point
 }
