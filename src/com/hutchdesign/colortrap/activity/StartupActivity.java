4af0c2ca0215f42f5e8ef2eadd7de65227cc3ff5
Allie Curry <allie516@gmail.com>
2014-03-11 17:49:27 -0500

    Added touch animation to menu

--- src/com/hutchdesign/colortrap/activity/StartupActivity.java
+++ src/com/hutchdesign/colortrap/activity/StartupActivity.java
@@ -1,11 +1,14 @@
 package com.hutchdesign.colortrap.activity;
 
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import com.hutchdesign.colortrap.R;
 import com.hutchdesign.colortrap.model.Mode;
 import com.hutchdesign.colortrap.util.FragmentUtility;
+import com.hutchdesign.colortrap.view.RotateTouchListener;
 
 public class StartupActivity extends Activity implements View.OnClickListener {
 
@@ -16,9 +19,30 @@ public class StartupActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
-        findViewById(R.id.button_new_computer).setOnClickListener(this);
-        findViewById(R.id.button_new_hotseat).setOnClickListener(this);
+        RotateTouchListener touchListener = getTouchAnimationListener();
+
+        final View buttonHotseat = findViewById(R.id.button_new_hotseat);
+        final View buttonComputer = findViewById(R.id.button_new_computer);
+        final View buttonHelp = findViewById(R.id.button_help);
+
+        buttonHotseat.setOnClickListener(this);
+        buttonComputer.setOnClickListener(this);
+
+        buttonHotseat.setOnTouchListener(touchListener);
+        buttonComputer.setOnTouchListener(touchListener);
+        buttonHelp.setOnTouchListener(touchListener);
+
         gameFragment = new GameFragment();
+    }
+
+    private RotateTouchListener getTouchAnimationListener() {
+        final ObjectAnimator animatorLeft = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.rotate_left);
+        final ObjectAnimator animatorRight = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.rotate_right);
+        final ObjectAnimator animatorLeftReset = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.rotate_left_reset);
+        final ObjectAnimator animatorRightReset = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.rotate_right_reset);
+        RotateTouchListener touchListener = new RotateTouchListener(animatorLeft, animatorRight, getResources().getInteger(R.integer.rotation_menu_duration));
+        touchListener.setResetAnimators(animatorLeftReset, animatorRightReset);
+        return touchListener;
     }
 
     private void startGame(Mode mode) {
