af729652001fd62497f9074a75d90ba4755bd955
hutch <mikehutch06@gmail.com>
2014-03-14 15:34:52 -0500

    Ad exception handling

--- src/com/hutchdesign/colortrap/activity/StartupActivity.java
+++ src/com/hutchdesign/colortrap/activity/StartupActivity.java
@@ -5,12 +5,14 @@ import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.hutchdesign.colortrap.R;
 import com.hutchdesign.colortrap.model.Mode;
 import com.hutchdesign.colortrap.util.FragmentUtility;
 import com.hutchdesign.colortrap.view.RotateTouchListener;
-import com.google.android.gms.ads.*;
 
 
 public class StartupActivity extends Activity implements View.OnClickListener {
@@ -18,6 +20,7 @@ public class StartupActivity extends Activity implements View.OnClickListener {
     private GameFragment gameFragment;
     private InterstitialAd interstitial;
     private static final String NEW_GAME_AD_ID = "ca-app-pub-4074371291605833/9344298102";
+    private static String TAG = StartupActivity.class.getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,14 +28,18 @@ public class StartupActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.menu);
 
         // Create the interstitial.
-        interstitial = new InterstitialAd(this);
-        interstitial.setAdUnitId(NEW_GAME_AD_ID);
+        try{
+            interstitial = new InterstitialAd(this);
+            interstitial.setAdUnitId(NEW_GAME_AD_ID);
 
-        // Create ad request.
-        AdRequest adRequest = new AdRequest.Builder().build();
+            // Create ad request.
+            AdRequest adRequest = new AdRequest.Builder().build();
 
-        // Begin loading your interstitial.
-        interstitial.loadAd(adRequest);
+            // Begin loading your interstitial.
+            interstitial.loadAd(adRequest);
+        }
+        catch(Exception adErr){ Log.e(TAG, adErr.toString()); }
+
         RotateTouchListener touchListener = getTouchAnimationListener();
 
         final View buttonHotseat = findViewById(R.id.button_new_hotseat);
@@ -72,7 +79,8 @@ public class StartupActivity extends Activity implements View.OnClickListener {
     }
 
     private void startGame(Mode mode) {
-        displayInterstitial();
+        try{ displayInterstitial(); }
+        catch(Exception adErr){ Log.e(TAG, adErr.toString()); }
         FragmentUtility.replaceFragment(this, gameFragment, R.id.fragment, null);
         gameFragment.startGame(this, mode);
 
